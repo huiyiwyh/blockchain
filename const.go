42a8a67b3db64ba07d99283f5fb8ecbeb1d7ffad
huiyiwyh <717729520@qq.com>
2019-02-26 16:27:05 +0800

    modify cli.go

--- const.go
+++ const.go
@@ -16,7 +16,7 @@ const blockversion = byte(0x00)
 const addressChecksumLen = 4
 
 // MempoolMaxTxs defers maxTxs num in mempool
-const mempoolMaxTxs = 2
+const mempoolMaxTxs = 50
 
 // BlockchaindbFile defers dbname
 const blockchaindbFile = "blockchain.db"
@@ -24,14 +24,13 @@ const blockchaindbFile = "blockchain.db"
 // blocksBucket defines the bucket of block in Blockchain
 const blocksBucket = "blocks"
 
+const isAllowedSideChainNum = 5
+
 // orphanBlocksBucket defines the bucket of orphan block
 const orphanBlocksBucket = "orphanblocks"
 
 // genesisCoinbaseData defines the genesisCoin baseData
 const genesisCoinbaseData = "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks"
-
-// isAllowedSideChainNum defines how many side chan will be saved
-const isAllowedSideChainNum = 5
 
 // subsidy defines when a block mind ,the miner can receive 20
 const subsidy = 20
